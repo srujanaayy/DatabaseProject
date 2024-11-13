545302e9a52d7ace4bc29cbbd3a1cf75c89456c2
medhakotra <161172307+medhakotra@users.noreply.github.com>
2024-11-13 06:39:46 -0600

    Update getdata1b.php

--- getdata1b.php
+++ getdata1b.php
@@ -1,15 +1,17 @@
 /* getdata1b.php */
 //need to add a update statement, database name and password
 ?php
-$eid = $_GET['EID'];
-$pwd = $_GET['Password'];
+
+$cid = $_GET['CustomerID']; //possible injection: 123' OR 1 = 1 #
+$pid = $_GET['PaymentID'];
+
 
 //new mysqli(name of host, mySQL username, password, name of database)
 $conn = new mysqli("localhost", "root", "password", "db");
 
-$sql = "UPDATE
-        SET
-        WHERE"
+$sql = "UPDATE Payment
+        SET ShippingFee = 0
+        WHERE CustomerID = '$cid' AND PaymentID = '$pid'"
 
 $result = $conn->query($sql);
 $conn->close();
