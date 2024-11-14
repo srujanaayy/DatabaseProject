fe58d4a027b8048545b2183ac4a5f98b0ff0eee7
srujanaayy <131008131+srujanaayy@users.noreply.github.com>
2024-11-14 10:34:06 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,27 @@
+const sql = require("mssql");
+require("dotenv").config();
+
+const config = {
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  options: {
+    encrypt: true,
+    trustServerCertificate: false,
+  },
+};
+const pool = new sql.ConnectionPool(config);
+const poolConnect = pool.connect();
+
+poolConnect
+  .then(() => {
+    console.log("Connected to SQL Server successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
+
+module.exports = { pool, poolConnect };
 
