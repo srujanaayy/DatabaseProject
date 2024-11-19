ac5ed03aeb02d6d853fff8a2d8a30eb1049fc468
medhakotra <161172307+medhakotra@users.noreply.github.com>
2024-11-18 21:00:13 -0600

    Create preparedStatement2.php

---
+++ preparedStatement2.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$db = "db";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+$sql = "SELECT CardNumber
+        FROM Customer
+        WHERE CustomerID = ? ";
+
+if($stmt = $conn->prepare($sql)) {
+  $stmt->bind_param("ss", $CustomerID);
+  $stmt->execute();
+  $stmt->bind_result($CardNumber);
+  while($stmt->fetch()) {
+    printf(%d \n", $CardNumber);
+    }
+}
