28bf21f5c0e50b127380147d1b594744690d3bf9
medhakotra <161172307+medhakotra@users.noreply.github.com>
2024-11-18 21:05:20 -0600

    Update preparedStatement2.php

--- preparedStatement2.php
+++ preparedStatement2.php
@@ -10,11 +10,14 @@ $sql = "SELECT CardNumber
         FROM Customer
         WHERE CustomerID = ? ";
 
+//initialize
+$CustomerID = 12345;
+
 if($stmt = $conn->prepare($sql)) {
-  $stmt->bind_param("ss", $CustomerID);
+  $stmt->bind_param("i", $CustomerID);
   $stmt->execute();
   $stmt->bind_result($CardNumber);
   while($stmt->fetch()) {
-    printf(%d \n", $CardNumber);
+    printf("%d \n", $CardNumber);
     }
 }
